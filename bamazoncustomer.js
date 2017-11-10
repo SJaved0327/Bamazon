b11200f51687381fe1aea12260c5dd576223e365
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2017-11-10 11:48:22 -0500

    setting up inquirer

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-//const inquirer = require("inquirer");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
 	host: "localhost",
@@ -12,4 +12,34 @@ const connection = mysql.createConnection({
 connection.connect((err) =>{
 	if (err) throw err;
 	console.log(`connected as id ${connection.threadId}`);
+	runCustomer();
 });
+
+function runCustomer() {
+	inquirer
+	    .prompt({
+	      name: "buy",
+	      type: "list",
+	      message: "What department are you browsing?",
+	      choices: [
+	        "Pets",
+	        "Home",
+	        "Cosmetics"
+	      ]
+	    })
+	    .then(function(answer) {
+	      switch (answer.buy) {
+	        case "Pets":
+	          petsSearch();
+	          break;
+
+	        case "Home":
+	          homeSearch();
+	          break;
+
+	        case "Cosmetics":
+	          cosmeticsSearch();
+	          break;
+      	}
+   	});
+}
